b8f1a1dbb46b2d92c6520e98cf13f6fcc45fd58b
Gustav <st3nsson@gmail.com>
2019-12-04 22:36:03 +0100

    added scaffolding for second data source

--- dayx/dayx.go
+++ dayx/dayx.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	fmt.Println("Result for data.txt")
 	start := time.Now()
 	task1 := Task1("data.txt")
 	fmt.Printf("task1: %d \n", task1)
@@ -15,4 +16,17 @@ func main() {
 	task2 := Task2("data.txt")
 	fmt.Printf("task2: %d \n", task2)
 	fmt.Println("task 2 finished in: ", time.Since(start))
+	fmt.Println("-----------------------------")
+
+	fmt.Println("Result for dataSpotify.txt")
+	start = time.Now()
+	task1Spotify := Task1("dataSpotify.txt")
+	fmt.Printf("task1: %d \n", task1Spotify)
+	fmt.Println("task 1 finished in: ", time.Since(start))
+
+	start = time.Now()
+	task2Spotify := Task2("dataSpotify.txt")
+	fmt.Printf("task2: %d \n", task2Spotify)
+	fmt.Println("task 2 finished in: ", time.Since(start))
+	fmt.Println("-----------------------------")
 }
